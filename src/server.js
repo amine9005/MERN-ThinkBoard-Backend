4bc912ea03cc2dd6571c27fcc3aac603f9511cab
amine9005 <amamselmitrader@gmail.com>
2025-11-09 00:53:01 +0100

    origin update

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://mern-thinkboard-frontend-embb.onrender.com"],
   })
 );
 app.use(express.json());
